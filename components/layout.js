459f5ef1ed125b2dd3192e4c8620545b565ea430
fand <fand.gmork@gmail.com>
2017-03-12 00:10:12 +0900

    Add styles for Layout component

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,39 @@
 import Head from 'next/head'
+import css from 'next/css'
 
 export default (props) => (
   <div>
     <Head>
       <title>{ props.title || '⚡ハロー、React⚡' }</title>
+      <style>{`
+        body { margin: 0; }
+      `}</style>
     </Head>
 
-    <div>
-      <header>
+    <div className={wrapperStyle}>
+      <header className={headerStyle}>
         ⚡ハロー、React⚡
       </header>
-      <div>
+      <div className={contentStyle}>
         { props.children }
       </div>
     </div>
   </div>
 )
+
+const wrapperStyle = css({
+  width: 960,
+  margin: '0 auto',
+  textAlign: 'center',
+})
+
+const headerStyle = css({
+  margin: '40px 0',
+  fontSize: '2.5em',
+  fontWeight: 'bold',
+  fontStyle: 'italic',
+})
+
+const contentStyle = css({
+  color: 'dimgray',
+})
