b801038615fe11c8fd8b20a65b6513a995932587
fand <fand.gmork@gmail.com>
2017-03-12 00:10:12 +0900

    Layoutコンポーネントにスタイルをあてる

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,4 @@
+import { css } from 'glamor'
 import Head from 'next/head'
 
 /**
@@ -11,15 +12,34 @@ export default (props) => (
     <Head>
       <title>{ props.title || '⚡ハロー、React⚡' }</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      <style jsx global>{`
+        body { margin: 0; }
+      `}</style>
     </Head>
 
-    <div>
-      <header>
+    <div className={wrapperStyle}>
+      <header className={headerStyle}>
         <h1>⚡ハロー、React⚡</h1>
       </header>
-      <div>
+      <div className={contentStyle}>
         { props.children }
       </div>
     </div>
   </div>
 )
+
+const wrapperStyle = css({
+  maxWidth: 960,
+  margin: '0 auto',
+  textAlign: 'center',
+})
+
+const headerStyle = css({
+  margin: '60px 0 40px',
+  fontStyle: 'italic',
+})
+
+const contentStyle = css({
+  color: 'dimgray',
+  margin: 10,
+})
