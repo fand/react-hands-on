41dc12148893de9292099928180b8208ee124293
fand <fand.gmork@gmail.com>
2017-03-12 15:09:02 +0900

    Hamburgerコンポーネントを動かす

--- components/hamburger.js
+++ components/hamburger.js
@@ -1,17 +1,52 @@
 import { css } from 'glamor'
+import React from 'react'
 
 /**
  * ハンバーガーボタンに対応するReactコンポーネント
  */
-export default () => (
-  <div className={hamburgerStyle}>
-    <div className="hamburger hamburger--squeeze">
-      <div className="hamburger-box">
-        <div className="hamburger-inner"></div>
+export default class Hamburger extends React.Component {
+
+  constructor () {
+    super()
+
+    // アクティブかどうかをisActiveで管理する
+    this.state = {
+      isActive: false,
+    }
+  }
+
+  /**
+   * isActiveを切り替える
+   */
+  toggle () {
+    this.setState({
+      isActive: !this.state.isActive,
+    })
+  }
+
+  render () {
+    return (
+      <div className={hamburgerStyle} onClick={() => this.toggle()}>
+        <div className={getClassName(this.state.isActive)}>
+          <div className="hamburger-box">
+            <div className="hamburger-inner"></div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+
+}
+
+/**
+ * active状態を受け取ってクラス名を返す
+ */
+const getClassName = (isActive) => {
+  return (isActive ?
+    'hamburger hamburger--squeeze is-active' : // アクティブな時
+    'hamburger hamburger--squeeze'             // アクティブじゃない時
+  )
+}
 
 const hamburgerStyle = css({
   position: 'fixed',
