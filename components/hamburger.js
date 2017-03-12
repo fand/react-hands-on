11fb275b25522cb4a6e2ac2a11526878fffc2b9e
fand <fand.gmork@gmail.com>
2017-03-12 15:09:02 +0900

    Add state to Hamburger component

--- components/hamburger.js
+++ components/hamburger.js
@@ -1,14 +1,44 @@
 import css from 'next/css'
+import React from 'react'
 
-export default (props) => (
-  <div className={hamburgerStyle}>
-    <div className="hamburger hamburger--squeeze">
-      <div className="hamburger-box">
-        <div className="hamburger-inner"></div>
+export default class hamburger extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      isActive: false
+    }
+  }
+
+  toggle () {
+    this.setState({
+      isActive: !this.state.isActive,
+    })
+  }
+
+  render () {
+    return (
+      <div className={hamburgerStyle} onClick={() => this.toggle()}>
+        <div className={getClassName(this.state.isActive)}>
+          <div className="hamburger-box">
+            <div className="hamburger-inner"></div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    );
+  }
+
+}
+
+const getClassName = (isActive) => {
+  let className = 'hamburger hamburger--squeeze'
+
+  if (isActive) {
+    className += ' is-active'
+  }
+
+  return className
+}
 
 const hamburgerStyle = css({
   position: 'fixed',
