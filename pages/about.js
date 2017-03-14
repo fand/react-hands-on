9317f5394af5e3cdf9b2572eb0af3f36307e7b7c
fand <fand.gmork@gmail.com>
2017-03-15 00:12:26 +0900

    Aboutページを追加

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * Aboutページに対応するReactコンポーネント
+ */
+export default () => (
+  <div>
+    <h1>About me</h1>
+
+    <p>
+      Aenean rhoncus augue at maximus pharetra. Nulla bibendum justo quis nisl faucibus volutpat. Aenean ornare dolor quam, ut molestie mi condimentum in. Proin turpis sapien, scelerisque sed convallis non, efficitur in eros. Sed non neque purus. Aliquam venenatis sagittis enim. Sed massa orci, dictum ut ante vitae, posuere imperdiet dui.
+    </p>
+  </div>
+)
